d04c8fb481af8e74dd5f554ee41e0b150ebed909
Damian <dmninbox@gmail.com>
2024-12-20 13:38:05 +0100

    select basic

--- scripts/01_learning_mysql_chapter03_01_query_primer.sql
+++ scripts/01_learning_mysql_chapter03_01_query_primer.sql
@@ -1,9 +1,8 @@
-SELECT 
-  emp_id, 
-  fname, 
-  lname 
-FROM 
-  employee;
-  
--- SQL Query Clauses 
+SELECT
+        emp_id,
+        fname ,
+        lname
+FROM
+        employee;
+-- SQL Query Clauses
 -- SELECT, FROM, WHERE, GROUP BY, HAVING, ORDER BY, LIMIT, and OFFSET
