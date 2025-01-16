8bfc1b9c7ff900c2b0c7566c3161966a88f2078e
Damian <dmninbox@gmail.com>
2025-01-16 15:45:20 +0100

    cross join

---
+++ scripts/01_learning_mysql_chapter05_02_join.sql
@@ -0,0 +1,11 @@
+-- This is called "cross join"
+
+SELECT e.fname, e.lname, d.name
+FROM 
+	employee AS e 
+	JOIN department AS d;
+	
+	-- Because the query does not specify how are tables joined
+	-- the database server generates CARTESIAN PRODUCT
+	-- which is permitation of the two tables (18 emp x 3 dep = 54 permutations)
+	-- This is called "cross join"
