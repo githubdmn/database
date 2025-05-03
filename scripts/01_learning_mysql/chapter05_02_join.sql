10acca1b5a5a93a637810c9f5f6975ed2b19fd30
githubdmn <dmninbox@gmail.com>
2025-05-03 15:37:43 +0200

    `CROSS JOIN` - `JOIN` explanation added
    `CROSS JOIN` - `JOIN`

--- scripts/01_learning_mysql/chapter05_02_join.sql
+++ scripts/01_learning_mysql/chapter05_02_join.sql
@@ -1,13 +1,15 @@
--- This is called "cross join"
+-- This is called "cross join" or just "join"
 
 SELECT e.fname, e.lname, d.name
 FROM 
 	employee AS e 
-	JOIN department AS d;
+	CROSS JOIN department AS d;
+    -- JOIN department as d;
 	
 	-- Because the query does not specify how are tables joined
 	-- the database server generates CARTESIAN PRODUCT
 	-- which is permitation of the two tables (18 emp x 3 dep = 54 permutations)
 	-- This is called "cross join"
+    -- Avoid mixing CROSS JOIN with ON—it’s non-standard and confusing.
 	
 	
