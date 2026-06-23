dc4a4d98dd5387c444ca5aadebaba4a90aa02250
dmn <dmninbox@gmail.com>
2026-06-23 12:18:48 +0200

    numeric data

---
+++ scripts/mysql/01_learning_mysql/chapter07_03_numeric_data.sql
@@ -0,0 +1,13 @@
+
+SELECT (37 * 59) / (78 - (8*6));
+SELECT MOD(10, 4);
+SELECT POW(2, 8);
+-- CEIL(72.445) FLOOR(72.445) 
+-- ROUND(72.0000001) ROUND(72.999999) ROUND(72.0909, 1)
+-- TRUNCATE(72.9090, 1) TRUNCATE(72.0909, 2)
+-- SIG(available_balance) -- 1 or -1 
+-- ABS(available_balance)
+--
+--
+--
+
